ea9f2dc5ec3a15aab75f04ab137cd43b0639986b
Joshua Mo <joshua.mo.876@gmail.com>
2025-04-20 17:51:33 +0100

    feat: satisfy clippy

--- examples/render_with_screenshot/main.rs
+++ examples/render_with_screenshot/main.rs
@@ -4,10 +4,7 @@ use mermaid_rs::Mermaid;
 fn main() {
     let mermaid = Mermaid::new().unwrap();
     let res = mermaid
-        .render_with_screenshot(
-            "graph TD;\nA[HTML Label] --> B[Regular Node];",
-            CaptureScreenshotFormatOption::Png,
-        )
+        .render_with_screenshot("graph TD;\nA[HTML Label] --> B[Regular Node];")
         .unwrap();
 
     std::fs::write("res.png", res).unwrap();
